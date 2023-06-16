31b7293ee309e95019510cfda2a489593bd3162d
white_key <112360339+Clue03@users.noreply.github.com>
2023-06-16 09:09:55 +0900

    Date_Person

---
+++ Date.h
@@ -0,0 +1,15 @@
+#ifndef DATE_H
+#define DATE_H
+class Date {
+private:
+	int month;
+	int day;
+	int year;
+public:
+	Date();
+	Date(int month, int day, int year);
+	Date(const Date& d);
+	~Date();
+	void print() const;
+};
+#endif // !DATE_H
